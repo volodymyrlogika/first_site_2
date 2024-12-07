43a81ed691a8e6044d63532c74dd3c0a490c550b
Volodymyr <volodymyr.logika@gmail.com>
2024-12-07 12:29:37 +0200

    07/12

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from db_scripts import DatabaseManager
 app = Flask(__name__)  # Створюємо веб–додаток Flask
 db = DatabaseManager("blog.db")
 
+@app.context_processor
+def get_categories():
+    categories = db.get_all_categories()
+    return dict(categories=categories)
+
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
     articles = db.get_all_articles()
@@ -15,6 +20,12 @@ def index():
 def article_page(article_id):
     article = db.get_article(article_id)
     return render_template("article_page.html", article=article)  # html-сторінка, що повертається у браузер
+
+
+@app.route("/categories/<int:category_id>")  # Вказуємо url-адресу для виклику функції
+def category_page(category_id):
+    articles = db.get_category_articles(category_id)
+    return render_template("index.html", articles=articles)  # html-сторінка, що повертається у браузер
 
 
 if __name__ == "__main__":
--- db_scripts.py
+++ db_scripts.py
@@ -27,3 +27,18 @@ class DatabaseManager:
         data = self.cursor.fetchone()
         self.close()
         return data
+
+    def get_all_categories(self):
+        self.open()
+        self.cursor.execute("""SELECT * FROM categories""")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    
+    def get_category_articles(self, category_id):
+        self.open()
+        self.cursor.execute("""SELECT * FROM articles WHERE category_id=?""", [category_id])
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    
